46c44fcd6bbe120da6220bdb5f32aa35dcf0b69a
adriana <adrianapenate98@gmail.com>
2019-12-02 13:19:40 +0000

    Red test - be_zero_when_they_are_zero

---
+++ src/test/java/DeafRatsOfHamelinShould.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class DeafRatsOfHamelinShould {
+    @Test
+    public void be_zero_when_they_are_zero() {
+        assertThat(DeafRatsOfHamelin.countDeafRats("P")).isEqualTo(0);
+    }
+
+    private static class DeafRatsOfHamelin {
+        public static int countDeafRats(String p) {
+            return -1;
+        }
+    }
+}
