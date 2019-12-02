0b380c91895a815851a0f1097933c3997a28b8e7
adriana <adrianapenate98@gmail.com>
2019-12-02 13:28:54 +0000

    Red test - be_one_when_it_is_facing_right

--- src/test/java/DeafRatsOfHamelinShould.java
+++ src/test/java/DeafRatsOfHamelinShould.java
@@ -9,6 +9,11 @@ public class DeafRatsOfHamelinShould {
         assertThat(DeafRatsOfHamelin.count("P")).isEqualTo(0);
     }
 
+    @Test
+    public void be_one_when_it_is_facing_right() {
+        assertThat(DeafRatsOfHamelin.count("P")).isEqualTo(1);
+    }
+
     private static class DeafRatsOfHamelin {
         public static int count(String rats) {
             return 0;
